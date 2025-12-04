363f1cf132dc8ef947d722cc7910473c11fbd1cc
Om Patki <om.y.patki@gmail.com>
2025-12-04 15:25:14 -0800

    adjusted camera for level 3

--- scripts/cameraControl.gd
+++ scripts/cameraControl.gd
@@ -23,3 +23,26 @@ func _on_diningRoomDoor_body_entered(body: PhysicsBody2D) -> void:
 		limit_bottom = 256
 		currCameraPos = 1
 		parent.position = Vector2(380, 176)
+		
+
+
+func _on_bedRoom_body_entered(body: PhysicsBody2D) -> void:
+	var parent = get_parent()
+
+	if currCameraPos == 1:
+		# Switch to Bedroom (Room 3)
+		limit_left = -255
+		limit_top = -100
+		limit_right = 1
+		limit_bottom = 291
+		currCameraPos = 3
+		parent.position = Vector2(-30, 176)
+
+	else:
+		# Switch back to Room 1
+		limit_left = 0
+		limit_top = 0
+		limit_right = 415
+		limit_bottom = 256
+		currCameraPos = 1
+		parent.position = Vector2(30, 176)
