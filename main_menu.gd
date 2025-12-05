b04cc1dc8fdd1f381f21a3f9eda6dccbc7c3b926
Raquib Alam <raquib.alam2023@gmail.com>
2025-12-04 17:08:35 -0800

    Inital Animations for the game
    Yay!

---
+++ main_menu.gd
@@ -0,0 +1,6 @@
+extends Control
+
+func _on_button_pressed():
+	# This loads your game scene.
+	# Make sure the path "res://game.tscn" matches your actual file name!
+	get_tree().change_scene_to_file("res://game.tscn")
