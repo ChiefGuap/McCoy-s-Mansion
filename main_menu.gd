e53975ee1db2b85c0de4c958b201ef926a31acc6
Raquib Alam <raquib.alam2023@gmail.com>
2025-12-06 18:05:19 -0800

    I hate godot
    - Worked on First cutscene animation for the game
    - utilzied new assets and sprites, and made trnasitions as well

--- main_menu.gd
+++ main_menu.gd
@@ -3,4 +3,4 @@ extends Control
 func _on_button_pressed():
 	# This loads your game scene.
 	# Make sure the path "res://game.tscn" matches your actual file name!
-	get_tree().change_scene_to_file("res://game.tscn")
+	get_tree().change_scene_to_file("res://intro_woods.tscn")
