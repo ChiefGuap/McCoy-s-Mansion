f52578918997860692a0047497619e4d363bf809
Andy Tran <anctran@ucdavis.edu>
2025-12-07 20:33:38 -0800

    Added collision, level indicator, and other fixes

--- scripts/cameraControl.gd
+++ scripts/cameraControl.gd
@@ -2,14 +2,15 @@ extends Camera2D
 
 var currCameraPos:int = 1
 
+@onready var level_label: Label = get_tree().root.find_child("LevelNameLabel", true, false)
+
 func _ready() -> void:
+	level_label.text = "LIVING ROOM"
 	pass 
 
 func _on_diningRoomDoor_body_entered(body: PhysicsBody2D) -> void:
-	# --- NEW: Check if the body is the Player ---
 	if body.name != "Player":
 		return
-	# --------------------------------------------
 
 	var parent = get_parent()
 	
@@ -20,6 +21,7 @@ func _on_diningRoomDoor_body_entered(body: PhysicsBody2D) -> void:
 		limit_bottom = 280
 		currCameraPos = 2
 		parent.position = Vector2(450, 170)
+		update_level_display("DINING ROOM")
 	else:
 		limit_left = 0
 		limit_top = 0
@@ -27,6 +29,7 @@ func _on_diningRoomDoor_body_entered(body: PhysicsBody2D) -> void:
 		limit_bottom = 276
 		currCameraPos = 1
 		parent.position = Vector2(380, 170)
+		update_level_display("LIVING ROOM")
 
 func _on_bedRoom_body_entered(body: PhysicsBody2D) -> void:
 	# --- NEW: Add the check here too just in case ---
@@ -44,6 +47,7 @@ func _on_bedRoom_body_entered(body: PhysicsBody2D) -> void:
 		limit_bottom = 313
 		currCameraPos = 3
 		parent.position = Vector2(-30, 170)
+		update_level_display("BED ROOM")
 
 	else:
 		# Switch back to Room 1
@@ -53,3 +57,9 @@ func _on_bedRoom_body_entered(body: PhysicsBody2D) -> void:
 		limit_bottom = 276
 		currCameraPos = 1
 		parent.position = Vector2(30, 170)
+		update_level_display("LIVING ROOM")
+	
+		
+func update_level_display(room_name: String) -> void:
+	if level_label:
+		level_label.text = room_name
